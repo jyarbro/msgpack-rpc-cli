ef2453e5910d47551f1030947a29438c7e171d3c
Jim Yarbro <yarbro@outlook.com>
2019-10-19 12:09:26 +0200

    Removed server code due to incompatibility with .NET Core. Removed deprecated .NET 4 code. Removed deprecated shared files.

--- src/MsgPack.Rpc.Server/GlobalSuppressions.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[module: SuppressMessage( "Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "MsgPack.Rpc.Server.Protocols.Filters" )]
